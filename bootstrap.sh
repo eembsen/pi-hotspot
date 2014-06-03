5035a926ca4f21e839ef453196f5035858edc0b7
rchrd <rchrd@users.noreply.github.com>
2014-06-03 20:06:39 +0200

    Lots of changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ set -e
 . ./bootstrap.conf
 
 echo 'Syncing Ansible roles'
-rsync -a -e "ssh -i ${PRIVATE_KEY}" --rsync-path='sudo rsync' ansible/roles pi@192.168.100.114:/etc/ansible/ > /dev/null 2>&1
+rsync -a --delete -e "ssh -i ${PRIVATE_KEY}" --rsync-path='sudo rsync' ansible/roles pi@192.168.100.114:/etc/ansible/ > /dev/null 2>&1
 echo 'Syncing Ansible config'
-rsync -a -e "ssh -i ${PRIVATE_KEY}" --rsync-path='sudo rsync' ansible/config/* pi@192.168.100.114:/etc/ansible/ > /dev/null 2>&1
+rsync -a --delete -e "ssh -i ${PRIVATE_KEY}" --rsync-path='sudo rsync' ansible/config/* pi@192.168.100.114:/etc/ansible/ > /dev/null 2>&1
 
 echo 'Installing sshpass (if needed)'
 ssh -i ${PRIVATE_KEY} ${USERNAME}@${IPADDRESS} 'if [ ! -f /usr/bin/sshpass ]; then sudo apt-get install sshpass; fi' > /dev/null 2>&1
--- readme.md
+++ readme.md
@@ -6,4 +6,28 @@
 2. Specify the location of the public key in `bootstrap.conf`
 3. Specify the the other configuration in `bootstrap.conf`
 4. If needed, change the `vars` section in `ansible/config/site.yml`
-5. If needed, change the credentials in `ansible/config/hosts`
+
+## Configuration
+Place the configuration in `ansible\config\host_vars\localhost.yml`
+This is an example of the configuration
+```
+---
+nameservers:
+  - 8.8.8.8
+  - 8.8.4.4
+local_network:
+  device: wlan0
+  macaddress: aa:aa:aa:aa:aa:aa
+  ipaddress: 192.168.1.1
+  dhcp:
+    start: 192.168.1.100
+    end: 192.168.1.199
+  ssid: RPI-local
+  passphrase: passphrase-local
+upstream_network:
+  device: wlan1
+  macaddress: aa:aa:aa:aa:aa:aa
+  ssid: RPI-upstream
+  passphrase: passphrase-upstream
+forward_device: eth0
+```
